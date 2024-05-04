c143dc20ade090c229282bd3cf77dc3bd608c662
yusuframazan <168287071+yusuframazan@users.noreply.github.com>
2024-05-04 22:32:44 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,33 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get } from "firebase/database";
+// Firebase veri tabanı URL'si
+var firebaseURL = "https://bitki-izleme-default-rtdb.firebaseio.com/sensors.json";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyABenFKvbnwgR4mcRGi-V5SQ4Y2alivx7w",
-  authDomain: "bitki-izleme.firebaseapp.com",
-  databaseURL: "https://bitki-izleme-default-rtdb.firebaseio.com",
-  projectId: "bitki-izleme",
-  storageBucket: "bitki-izleme.appspot.com",
-  messagingSenderId: "561641375328",
-  appId: "1:561641375328:web:f00fcc7054b32d36c86505",
-  measurementId: "G-N0QXWTRZVF"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-// Referansı oluştur
-const sensorsRef = ref(database, 'sensors');
-
-// Verileri oku
-get(sensorsRef).then((snapshot) => {
-  if (snapshot.exists()) {
-    console.log("Firebase'den alınan veriler:", snapshot.val());
-  } else {
-    console.log("Veri bulunamadı");
-  }
-}).catch((error) => {
-  console.error("Veriler alınırken hata oluştu:", error);
-});
+// Verileri çekmek için bir HTTP isteği yap
+fetch(firebaseURL)
+  .then(response => response.json())
+  .then(data => {
+    // Verileri işle
+    console.log("Firebase'den alınan veriler:", data);
+    
+    // Verileri HTML'e ekleme veya başka bir işlem yapma
+  })
+  .catch(error => {
+    console.error("Veriler alınırken hata oluştu:", error);
+  });
